b68211ee3d7677c6e6eb261094062944908113b8
Hector <hmreluz@gmail.com>
2018-02-11 18:31:00 -0500

    Validation of access politic with regression test with TDD

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,9 +13,12 @@
     @foreach($post->latestComments as $comment)
         <article class="{{ $comment->answer ? 'answer' : '' }}">
             {{ $comment->comment  }}
-            {!! Form::open(['route' => ['comments.accept', $comment],'method' => 'POST']) !!}
-                <button type="submit">Aceptar Respuesta</button>
-            {!! Form::close() !!}
+
+            @if(Gate::allows ('accept', $comment) && !$comment->answer)
+                {!! Form::open(['route' => ['comments.accept', $comment],'method' => 'POST']) !!}
+                    <button type="submit">Aceptar Respuesta</button>
+                {!! Form::close() !!}
+            @endif
         </article>
     @endforeach
 @endsection
