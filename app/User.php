6e48d2a429d73253ef6922b5c3b20427e763bb19
Hector <hmreluz@gmail.com>
2018-02-11 01:25:42 -0500

    Development access policy with TDD

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -43,5 +44,10 @@ class User extends Authenticatable
             'post_id' => $post->id
         ]);
         return $this->comments()->save($comment);
+    }
+
+    public function owns(Model $model)
+    {
+        return $this->id === $model->user_id;
     }
 }
